c393c21f8f681b4cb598c493f12f28b3e6db7c47
Tomas Tamantini <tomasbft@gmail.com>
2021-11-11 15:37:41 -0300

    Write explanatory comment

--- socket_server/consumer.py
+++ socket_server/consumer.py
@@ -3,7 +3,7 @@ from typing import Hashable, Protocol
 
 class Consumer(Protocol):
     async def handle_message(self, user_id: Hashable, message: str) -> None:
-        ...
+        """Handle incoming message from client"""
 
     async def handle_client_connected(self, user_id: Hashable) -> None:
         ...

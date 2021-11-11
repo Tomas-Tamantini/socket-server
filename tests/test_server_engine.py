c7e02cc41e1264ba569032b7bca0f5b94cca05f6
Tomas Tamantini <tomasbft@gmail.com>
2021-11-11 14:56:52 -0300

    Test run_producer method

--- tests/test_server_engine.py
+++ tests/test_server_engine.py
@@ -1,7 +1,8 @@
+from typing import Hashable, List, Tuple, Generator
+
 import pytest
 from socket_server.server_engine import ServerEngine
 from socket_server.socket_manager import SocketManager
-from typing import Hashable
 
 
 class MockConsumer:
@@ -18,6 +19,16 @@ class MockConsumer:
         pass
 
 
+class MockProducer:
+    def __init__(self, notifications: List[Tuple[Hashable, str]]) -> None:
+        self.__notifications = notifications
+
+    @property
+    async def notifications(self) -> Generator[Tuple[Hashable, str], None, None]:
+        for n in self.__notifications:
+            yield n
+
+
 @pytest.mark.asyncio
 async def test_messages_are_passed_to_consumer(socket_factory):
     consumer = MockConsumer()
@@ -27,3 +38,20 @@ async def test_messages_are_passed_to_consumer(socket_factory):
     socket = socket_factory(requests=sample_requests)
     await server.run_consumer(socket)
     assert consumer.received_requests == stored_requests
+
+
+@pytest.mark.asyncio
+async def test_notifications_are_sent_to_proper_sockets(socket_factory):
+    socket_manager = SocketManager()
+    socket_a = socket_factory()
+    user_a = socket_manager.register(socket_a)
+    socket_b = socket_factory()
+    user_b = socket_manager.register(socket_b)
+    notifications = [(user_a, 'first_msg_to_a'),
+                     (user_b, 'first_msg_to_b'), (user_a, 'second_msg_to_a')]
+    producer = MockProducer(notifications)
+    server = ServerEngine(socket_manager, producer=producer)
+    await server.run_producer()
+    assert socket_a.received_notifications == [
+        'first_msg_to_a', 'second_msg_to_a']
+    assert socket_b.received_notifications == ['first_msg_to_b']

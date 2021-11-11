26508e817f264fba9a7950b229fca86d1aead376
Tomas Tamantini <tomasbft@gmail.com>
2021-11-11 15:38:35 -0300

    Use ServerEngine instance

--- socket_server/run_server.py
+++ socket_server/run_server.py
@@ -6,29 +6,33 @@ import websockets
 
 from .consumer import Consumer
 from .producer import Producer
+from .server_engine import ServerEngine
+from .socket_manager import SocketManager
+
 
 async def run_server(host_address: str = 'localhost', port: int = 8080,
                      consumer: Optional[Consumer] = None, producer: Optional[Producer] = None):
     """
     Runs server at given host address and port.
     :param consumer: Handles incoming messages from the clients
-    :param producer: Produces outgoing messages to the clients
+    :param producer: Produces outgoing messages to the clients. If not provided, and consumer is provided, 
+                     and consumer implements Producer interface, then consumer will be used as producer.
     """
-    async with websockets.serve(partial(_gather_tasks, consumer=consumer, producer=producer), host_address, port):
+    server_engine = _initialize_server_engine(consumer, producer)
+    async with websockets.serve(partial(_gather_tasks, server_engine=server_engine), host_address, port):
         await asyncio.Future()
 
 
-async def _gather_tasks(socket, path, consumer: Optional[Consumer], producer: Optional[Producer]):
-    consumer_task = asyncio.ensure_future(_consumer_handler(socket, consumer))
-    producer_task = asyncio.ensure_future(_producer_handler(socket, producer))
+def _initialize_server_engine(consumer: Optional[Consumer], producer: Optional[Producer]) -> ServerEngine:
+    if consumer is not None and producer is None and isinstance(consumer, Producer):
+        producer = consumer
+    socket_manager = SocketManager()
+    return ServerEngine(socket_manager, consumer, producer)
+
+
+async def _gather_tasks(socket, path, server_engine: ServerEngine):
+    consumer_task = asyncio.ensure_future(server_engine.run_consumer(socket))
+    producer_task = asyncio.ensure_future(server_engine.run_producer(socket))
     _, pending = await asyncio.wait([consumer_task, producer_task], return_when=asyncio.FIRST_COMPLETED)
     for task in pending:
         task.cancel()
-
-
-async def _consumer_handler(socket, consumer: Optional[Consumer]):
-    pass
-
-
-async def _producer_handler(socket, producer: Optional[Producer]):
-    pass

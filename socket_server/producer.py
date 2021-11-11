3ae8d7eb687d784e91691c5c86423b939038e233
Tomas Tamantini <tomasbft@gmail.com>
2021-11-11 15:38:02 -0300

    Make producer runtime_checkable

--- socket_server/producer.py
+++ socket_server/producer.py
@@ -1,7 +1,8 @@
-from typing import Generator, Hashable, Protocol, Tuple
+from typing import Generator, Hashable, Protocol, Tuple, runtime_checkable
 
 
+@runtime_checkable
 class Producer(Protocol):
     @property
     async def notifications(self) -> Generator[Tuple[Hashable, str], None, None]:
-        """Async generatod that yields tuples with (recipient client, message)"""
+        """Async generator that yields tuples with (recipient client, message)"""

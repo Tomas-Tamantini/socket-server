13ab69dfd3b562a8f73c2e893d19c8ff358bc8ac
Tomas Tamantini <tomasbft@gmail.com>
2021-11-11 11:55:16 -0300

    Implement register method

--- socket_server/socket_manager.py
+++ socket_server/socket_manager.py
@@ -1,4 +1,23 @@
 class SocketManager:
+    def __init__(self) -> None:
+        self.__current_id = 0
+        self.__sockets_to_ids = {}
+        self.__ids_to_sockets = {}
+
     @property
     def num_clients(self) -> int:
-        return 0
+        return len(self.__sockets_to_ids)
+
+    def register(self, socket) -> None:
+        if socket in self.__sockets_to_ids:
+            raise self.SocketAlreadyRegisteredError(socket, self.__sockets_to_ids[socket])
+        self.__sockets_to_ids[socket] = self.__current_id
+        self.__ids_to_sockets[self.__current_id] = socket
+        self.__current_id += 1
+
+    # Exceptions:
+
+    class SocketAlreadyRegisteredError(Exception):
+        def __init__(self, socket, socket_id) -> None:
+            msg = f"Socket with id {socket_id} is already registered - {str(socket)}"
+            super().__init__(msg)

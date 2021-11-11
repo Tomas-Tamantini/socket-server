d8b759eb91200fa6ff80b10f97c16aadf6a47c10
Tomas Tamantini <tomasbft@gmail.com>
2021-11-11 14:40:42 -0300

    Create ServerEngine class, and imp. run_consumer

---
+++ socket_server/server_engine.py
@@ -0,0 +1,50 @@
+from typing import Optional
+
+from .consumer import Consumer
+from .producer import Producer
+from .socket_manager import SocketManager
+
+from websockets import ConnectionClosedError, ConnectionClosedOK, ConnectionClosed
+
+DISCONNECT_EXCEPTIONS = (ConnectionClosedError,
+                         ConnectionClosedOK, ConnectionClosed)
+
+
+class ServerEngine:
+    def __init__(self, socket_manager: SocketManager, consumer: Optional[Consumer] = None, producer: Optional[Producer] = None):
+        self.__socket_manager = socket_manager
+        self.__consumer = consumer
+        self.__producer = producer
+
+    async def run_consumer(self, socket):
+        try:
+            async for request in socket:
+                await self.__handle_request(socket, request)
+        except DISCONNECT_EXCEPTIONS:
+            await self.__unregister_socket(socket)
+        finally:
+            await self.__unregister_socket(socket)
+
+    async def run_producer(self):
+        async for user_id, message in self.__producer.notifications:
+            socket = self.__socket_manager.get_socket(user_id)
+            if socket is not None:
+                await self.__send_message(socket, message)
+
+    async def __handle_request(self, socket, request: str) -> None:
+        client_id = self.__socket_manager.get_client_id(socket)
+        if client_id is None:
+            client_id = self.__socket_manager.register(socket)
+            if self.__consumer is not None:
+                await self.__consumer.handle_client_connected(client_id)
+
+        if self.__consumer is not None:
+            await self.__consumer.handle_message(client_id, request)
+
+    async def __unregister_socket(self, socket) -> None:
+        client_id = self.__socket_manager.get_client_id(socket)
+        if client_id is None:
+            return
+        if self.__consumer is not None:
+            await self.__consumer.handle_client_disconnected(client_id)
+        self.__socket_manager.unregister(client_id)

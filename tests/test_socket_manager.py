ca4321ca25a637685600a1d93f2c131f1554e96f
Tomas Tamantini <tomasbft@gmail.com>
2021-11-11 11:55:03 -0300

    Test register method

--- tests/test_socket_manager.py
+++ tests/test_socket_manager.py
@@ -1,6 +1,22 @@
+import pytest
 from socket_server.socket_manager import SocketManager
 
 
 def test_manager_starts_empty():
     manager = SocketManager()
     assert manager.num_clients == 0
+
+
+def test_socket_can_register(socket_factory):
+    manager = SocketManager()
+    socket = socket_factory()
+    manager.register(socket)
+    assert manager.num_clients == 1
+
+
+def test_same_socket_cannot_register_twice(socket_factory):
+    manager = SocketManager()
+    socket = socket_factory()
+    manager.register(socket)
+    with pytest.raises(SocketManager.SocketAlreadyRegisteredError):
+        manager.register(socket)
